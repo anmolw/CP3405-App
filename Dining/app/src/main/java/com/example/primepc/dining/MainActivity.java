894ebb552efc67cc8cd779b61f9b794da7127e0a
carmeljeeva <carmeljeeva.j@gmail.com>
2018-08-06 12:46:58 +0800

    App_dev_stage

---
+++ Dining/app/src/main/java/com/example/primepc/dining/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.primepc.dining;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void menu_login(View view){
+
+        Intent myIntent = new Intent(this, MenuActivity.class);
+
+        this.startActivity(myIntent);
+
+
+    }
+}
---
+++ Dining/app/src/main/java/com/example/primepc/dining/MenuActivity.java
@@ -0,0 +1,21 @@
+package com.example.primepc.dining;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by PRIMEPC on 6/8/2018.
+ */
+
+public class MenuActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+    }
+
+
+
+
+}
