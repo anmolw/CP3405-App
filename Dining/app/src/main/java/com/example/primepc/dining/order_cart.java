fe9cc6e98d1d3546d277cf321fd269015a4fe14a
Tanuj Shah <tanujtushar.shah@my.jcu.edu.au>
2018-08-12 16:53:08 +0800

    Basic UI Created
    Basic UI Created

---
+++ Dining/app/src/main/java/com/example/primepc/dining/order_cart.java
@@ -0,0 +1,13 @@
+package com.example.primepc.dining;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class order_cart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_cart);
+    }
+}
